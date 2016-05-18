bee1017d74cfdc7bca9367f5c41223b7cb31fc2f
Thijs van Dijk <github@inurbanus.nl>
2016-05-18 14:13:07 +0200

    Add nice formatting of the certificate chain

--- bin/sslprobe/sslprobe.go
+++ bin/sslprobe/sslprobe.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/rsa"
 	"crypto/x509"
 	"flag"
 	"fmt"
@@ -102,7 +105,8 @@ func main() {
 				fmt.Printf("   %2d %s: %s\n", i, tc.Red("error"), err)
 				continue
 			}
-			fmt.Printf("   %2d subject: %s\n      issuer:  %s\n", i, cert.Subject, cert.Issuer)
+			subj, iss := prettyCertificate(cert)
+			fmt.Printf("   %2d %s\n      %s\n", i, subj, iss)
 		}
 	}
 
@@ -174,4 +178,100 @@ func cStrength(bits int) func(string) string {
 	} else {
 		return tc.Bred
 	}
+}
+
+func prettyCertificate(cert *x509.Certificate) (string, string) {
+	key := "unknown"
+	if cert.PublicKeyAlgorithm == x509.RSA {
+		pk, ok := cert.PublicKey.(*rsa.PublicKey)
+		if !ok {
+			key = tc.Bred("RSA - error")
+		} else {
+			col := cStrength(pk.N.BitLen())
+			key = col(fmt.Sprintf("RSA-%d", pk.N.BitLen()))
+		}
+	} else if cert.PublicKeyAlgorithm == x509.DSA {
+		pk, ok := cert.PublicKey.(*dsa.PublicKey)
+		if !ok {
+			key = tc.Bred("DSA - error")
+		} else {
+			bl := pk.P.BitLen()
+			col := tc.Red
+			if bl < 1536 {
+				col = tc.Bred
+			}
+			key = col(fmt.Sprintf("DSA-%d", bl))
+		}
+	} else if cert.PublicKeyAlgorithm == x509.ECDSA {
+		pk, ok := cert.PublicKey.(*ecdsa.PublicKey)
+		if !ok {
+			key = tc.Bred("ECDSA - error")
+		} else {
+			bl := pk.Params().P.BitLen()
+			col := tc.Green
+			if bl < 224 {
+				col = tc.Red
+			} else if bl < 256 {
+				col = tc.Yellow
+			}
+			key = col(fmt.Sprintf("ECDSA-%d", bl))
+		}
+	}
+
+	sig := strSigAlg(cert.SignatureAlgorithm)
+	if cert.SignatureAlgorithm == x509.UnknownSignatureAlgorithm ||
+		cert.SignatureAlgorithm == x509.MD2WithRSA ||
+		cert.SignatureAlgorithm == x509.MD5WithRSA {
+		sig = tc.Bred(sig)
+	} else if cert.SignatureAlgorithm == x509.SHA1WithRSA ||
+		cert.SignatureAlgorithm == x509.DSAWithSHA1 ||
+		cert.SignatureAlgorithm == x509.DSAWithSHA256 ||
+		cert.SignatureAlgorithm == x509.ECDSAWithSHA1 {
+		sig = tc.Red(sig)
+	} else {
+		sig = tc.Green(sig)
+	}
+
+	subject := cert.Subject.CommonName
+	if len(subject) > 45 {
+		subject = subject[0:45]
+	}
+	issuer := cert.Issuer.CommonName
+	if len(issuer) > 45 {
+		issuer = issuer[0:45]
+	}
+
+	subject = fmt.Sprintf("subject: %-45s  key type:  %s", subject, key)
+	issuer = fmt.Sprintf("issuer:  %-45s  signature: %s", issuer, sig)
+
+	return subject, issuer
+}
+
+func strSigAlg(s x509.SignatureAlgorithm) string {
+	if s == x509.MD2WithRSA {
+		return "MD2-RSA"
+	} else if s == x509.MD5WithRSA {
+		return "MD5-RSA"
+	} else if s == x509.SHA1WithRSA {
+		return "SHA1-RSA"
+	} else if s == x509.SHA256WithRSA {
+		return "SHA256-RSA"
+	} else if s == x509.SHA384WithRSA {
+		return "SHA384-RSA"
+	} else if s == x509.SHA512WithRSA {
+		return "SHA512-RSA"
+	} else if s == x509.DSAWithSHA1 {
+		return "SHA1-DSA"
+	} else if s == x509.DSAWithSHA256 {
+		return "SHA256-DSA"
+	} else if s == x509.ECDSAWithSHA1 {
+		return "SHA1-ECDSA"
+	} else if s == x509.ECDSAWithSHA256 {
+		return "SHA256-ECDSA"
+	} else if s == x509.ECDSAWithSHA384 {
+		return "SHA384-ECDSA"
+	} else if s == x509.ECDSAWithSHA512 {
+		return "SHA512-ECDSA"
+	}
+	return "UnknownSignatureAlgorithm"
 }
